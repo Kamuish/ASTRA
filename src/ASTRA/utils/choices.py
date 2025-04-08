0b33736e4bd2b55f1235f8b54f3671802cbe5615
Kamuish <andremiguel952@gmail.com>
2025-04-08 23:14:52 +0100

    user configurations and associated tests

---
+++ src/ASTRA/utils/choices.py
@@ -0,0 +1,81 @@
+from enum import Enum
+
+
+class DISK_SAVE_MODE(Enum):
+    """Enumerator to represent the DISK save mode of a given ASTRA object."""
+
+    DISABLED = 1
+
+    BASIC = 2
+
+    EXTREME = 3
+
+
+class WORKING_MODE(Enum):
+    ONE_SHOT = "ONE_SHOT"
+    ROLLING = "ROLLING"
+
+
+class TELLURIC_EXTENSION(Enum):
+    LINES = "LINES"
+    WINDOW = "WINDOW"
+
+
+class TELLURIC_CREATION_MODE(Enum):
+    tapas = "tapas"
+    telfit = "telfit"
+    OHemission = "OHemission"
+
+
+class STELLAR_CREATION_MODE(Enum):
+    Sum = "Sum"
+    Concatenate = "Concatenate"
+    OBSERVATION = "OBSERVATION"
+    Median = "Median"
+    PHOENIX = "PHOENIX"
+
+
+class TELLURIC_APPLICATION_MODE(Enum):
+    removal = "removal"
+    correction = "correction"
+
+
+class SPECTRA_INTERPOL_MODE(Enum):
+    """Enumerator to represent the DISK save mode of a given SBART object."""
+
+    SPLINES = 1
+    GP = 2
+
+
+class SPLINE_INTERPOL_MODE(Enum):
+    """Enumerator to represent the DISK save mode of a given SBART object."""
+
+    CUBIC_SPLINE = 1
+    QUADRATIC_SPLINE = 2
+    PCHIP = 3
+    NEAREST = 4
+    RBF = 5
+    SMOOTH_CUBIC_SPLINE = 6
+
+
+class FLUX_SMOOTH_CONFIGS(Enum):
+    """Enumerator to flux smoothing applied at template construction"""
+
+    NONE = 1
+    SAVGOL = 2
+
+
+class FLUX_SMOOTH_ORDER(Enum):
+    """The order in which the smooth is applied"""
+
+    BEFORE = 1
+    AFTER = 2
+    BOTH = 3
+
+
+class INTERPOLATION_ERR_PROP(Enum):
+    """how should we propagate uncertainties"""
+
+    interpolation = 1
+    propagation = 2
+    none = 3
