45ecb323a9d5d98286e9565411e02598f6038546
Kamuish <andremiguel952@gmail.com>
2025-12-04 17:56:14 +0000

    Fix issue with initialization of ESO based pipelines in v1.2.2

--- src/ASTRA/Instruments/ESO_PIPELINE.py
+++ src/ASTRA/Instruments/ESO_PIPELINE.py
@@ -149,7 +149,7 @@ class ESO_PIPELINE(Frame):
         if not self._internal_configs["use_old_pipeline"]:
             logger.debug("Checking for validity of file reduced with new pipeline")
             for key in ["S2D_A", "S2D_BLAZE_A", "S2D_BLAZE_TELL_CORR_A", "S2D_TELL_CORR_A", "S1D_A"]:
-                if key in self.file_path.stem():
+                if key in self.file_path.stem:
                     break
             else:
                 raise custom_exceptions.InvalidConfiguration(f"{self.name} can't recognize {self.file_path}")
