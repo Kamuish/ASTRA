03721e7b51afb365af3505e315f0c84660eaa340
Kamuish <andremiguel952@gmail.com>
2025-12-22 10:14:38 +0000

    Update multiprocessing fork to spawn, to avoid possible future issues

--- src/ASTRA/__init__.py
+++ src/ASTRA/__init__.py
@@ -5,4 +5,8 @@ version = "1.2.4"
 __version__ = version.replace(".", "-")
 __version_info__ = (int(i) for i in __version__.split("-"))
 
+import multiprocessing as mp
+
 from ASTRA.utils.create_logger import astra_logger, setup_ASTRA_logger
+
+mp.set_start_method("spawn", force=True)
